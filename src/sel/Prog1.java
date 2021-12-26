8381a84ef606886e123a57cc086f47c7e5708ebc
hp <hp@LAPTOP-LAHFBGA4>
2021-12-26 10:01:51 +0530

    for loop added

--- src/sel/Prog1.java
+++ src/sel/Prog1.java
@@ -6,6 +6,11 @@ public class Prog1 {
 		System.out.println("Hello world");
 		System.out.println("how are all of you");
 		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
 	}
 
 }
